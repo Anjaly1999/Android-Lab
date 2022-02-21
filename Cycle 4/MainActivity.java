fc60b303778f83c6a4696a10a9d54323db7bb76a
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2022-02-21 15:42:09 +0530

    Listview Toast

--- Cycle 4/MainActivity.java
+++ Cycle 4/MainActivity.java
@@ -1,34 +1,45 @@
-package com.example.sjcet.message;
+package com.example.sjcet.toast;
+
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    EditText textBox;
-    Button passButton;
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    TextView tv;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.tv);
 
-        textBox = (EditText)findViewById(R.id.textBox);
-        passButton = (Button)findViewById(R.id.passButton);
+        String msg=getIntent().getStringExtra("mykey");// receiving the data
+        tv.setText("welcome"+msg);
+        lv=(ListView)findViewById(R.id.lv);// display(welcom changing to admin)
+        // listvivew operation
+        String[] courses={"MCA","MBA","BBA","BCA"};
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,courses);
+        lv.setAdapter(adapter);
+        // attach listener
+        lv.setOnItemClickListener(this);//implement the method by alt+enter
+    }
 
-        passButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String str = textBox.getText().toString();
 
-                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-                intent.putExtra("message", str);
 
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String itemname=lv.getItemAtPosition(i).toString();
+        Toast.makeText(this,itemname,Toast.LENGTH_SHORT).show();
+
     }
 }
